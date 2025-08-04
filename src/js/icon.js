51aa36ba57d080f7305b6fd892a6637d55c6ac03
Lucas Ferreira <155773773+LuksDevs@users.noreply.github.com>
2025-08-04 17:40:53 -0300

    .

---
+++ src/js/icon.js
@@ -0,0 +1,20 @@
+
+const urlIcon = "http://127.0.0.1:5500/src/json/icons.json";
+
+export async function getIcons() {
+    const iconsResponse = await fetch(urlIcon);
+    const containerIcon = document.querySelector('.container-icon-card');
+    try {
+        const icons = await iconsResponse.json();
+        icons.map((icon) => {
+            containerIcon.innerHTML +=
+                `
+                <span class="span-icon">
+                    <i class='${icon.icon}'></i> ${icon.description}
+                </span>
+            `
+        })
+    } catch (err) {
+        console.error(`Erro ao recuperar os dados dos icons de Tecnologias favoritas! Error: ${err}`)
+    }
+}
--- src/js/test.js
+++ src/js/test.js
@@ -1,25 +1,3 @@
-
-const urlIcon = "http://127.0.0.1:5500/src/json/icons.json";
-
-async function getIcons() {
-    const iconsResponse = await fetch(urlIcon);
-    const containerIcon = document.querySelector('.container-icon-card');
-    try {
-        const icons = await iconsResponse.json();
-        icons.map((icon) => {
-            containerIcon.innerHTML +=
-                `
-                <span class="span-icon">
-                    <i class='${icon.icon}'></i> ${icon.description}
-                </span>
-            `
-        })
-    } catch (err) {
-        console.error(`Erro ao recuperar os dados dos icons de Tecnologias favoritas! Error: ${err}`)
-    }
-}
-getIcons()
-
 // ============== Carousel ==============
 document.addEventListener('DOMContentLoaded', () => {
     const carousel = document.querySelector('.carousel');
